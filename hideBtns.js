30bdb02313aed689bf4906c1c71e488db6ac15c8
Michael-Knopf <knopfwebdevelopment@gmail.com>
2021-09-28 20:40:44 -0400

    add fuctionality to toggle btn; add hide app function

---
+++ hideBtns.js
@@ -0,0 +1,23 @@
+const app = document.querySelector(".app")
+const inactiveTime = 60000;
+
+let mouseLastMoved = new Date();
+
+document.addEventListener('mousemove', () => {
+    mouseLastMoved = new Date();
+    app.classList.remove("inactive")
+})
+
+function deactivateApp() {
+    let now = new Date();
+    let deltaTime = now - mouseLastMoved;
+
+    if (deltaTime >= inactiveTime) {
+        app.classList.add("inactive")
+        document.body.style.cursor = "none"
+    }
+
+    requestAnimationFrame(deactivateApp)
+}
+
+deactivateApp()
